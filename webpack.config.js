df5e552a5a91855bcf3ed364ccd3f75328f6e7e3
ryazanov-alexandr <ryazanov.a1exandr@yandex.ru>
2024-05-10 15:34:34 +0300

    another way fix env variables

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,9 @@ module.exports = {
         new MiniCssExtractPlugin({
           filename: '[name].[contenthash].css',
         }),
-        new Dotenv(),      
+        new Dotenv({
+          systemvars: true
+        }),      
       ),
     module: {
         rules: [
